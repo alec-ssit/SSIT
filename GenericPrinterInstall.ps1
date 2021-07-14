28c36729d866bf3ec921569357026696fe573144
alec-ssit <86682092+alec-ssit@users.noreply.github.com>
2021-07-14 10:04:33 -0400

    Add files via upload

---
+++ GenericPrinterInstall.ps1
@@ -0,0 +1,40 @@
+﻿# This script works on Windows 8 or newer since the add-printer cmdlets are't available on Windows 7.
+#  Download the HP Univeral Printing PCL 6 driver.
+
+# To find\extract the .inf file, run 7-zip on the print driver .exe and go to the folder in Powershell and run this command: get-childitem *.inf* |copy-item -destination "C:\examplefolder" Otherwise it's hard to find the .inf files.
+
+#Define a "shortName" variable and load all driver files into a [shortname].zip folder. Put driver zip file into same folder as this script
+
+$shortName="easytotypenamehere"
+$driver = "Full Name of the Driver Here"
+$address = "0.0.0.0"
+$name = "Printer Name as it Appears in Devices & Printers Here"
+$sleep = "3"
+$infFile = "driverfilehere.inf"
+
+Get-Location
+Expand-Archive -Path "$($PSScriptRoot)\$($shortName).zip" -DestinationPath "C:\Support\$($shortName)_Drivers"
+
+# The invoke command can be added to specify a remote computer by adding -computername. You would need to copy the .inf file to the remote computer first though. 
+# This script has it configured to run on the local computer that needs the printer.
+# The pnputil command imports the .inf file into the Windows driverstore. 
+# The .inf driver file has to be physically on the local or remote computer that the printer is being installed on.
+
+Invoke-Command {pnputil.exe -a "C:\Support\$($shortname)_Drivers\$($infFile)" }
+
+
+Add-PrinterDriver -Name $driver
+
+Start-Sleep $sleep
+
+# This creates the TCP\IP printer port. It also will not use the annoying WSD port type that can cause problems. 
+# WSD can be used by using a different command syntax though if needed.
+
+Add-PrinterPort -Name $address -PrinterHostAddress $address
+
+start-sleep $sleep
+
+Add-Printer -DriverName $driver -Name $name -PortName $address
+
+
+Start-Sleep $sleep 
---
+++ GenericSharepointSiteSync.ps1
@@ -0,0 +1,99 @@
+﻿#region Functions
+function Sync-SharepointLocation {
+    param (
+        [guid]$siteId,
+        [guid]$webId,
+        [guid]$listId,
+        [mailaddress]$userEmail,
+        [string]$webUrl,
+        [string]$webTitle,
+        [string]$listTitle,
+        [string]$syncPath,
+        [string]$regPath,
+        [string]$tenantTitle
+    )
+    try {
+         Add-Type -AssemblyName System.Web
+        #Encode site, web, list, url & email
+        [string]$siteId = [System.Web.HttpUtility]::UrlEncode($siteId)
+        [string]$webId = [System.Web.HttpUtility]::UrlEncode($webId)
+        [string]$listId = [System.Web.HttpUtility]::UrlEncode($listId)
+        [string]$userEmail = [System.Web.HttpUtility]::UrlEncode($userEmail)
+        [string]$webUrl = [System.Web.HttpUtility]::UrlEncode($webUrl)
+        #build the URI
+        $uri = New-Object System.UriBuilder
+        $uri.Scheme = "odopen"
+        $uri.Host = "sync"
+        $uri.Query = "siteId=$siteId&webId=$webId&listId=$listId&userEmail=$userEmail&webUrl=$webUrl&listTitle=$listTitle&webTitle=$webTitle"
+        #launch the process from URI
+        Write-Host $uri.ToString()
+        start-process -filepath $($uri.ToString())
+    }
+    catch {
+        $errorMsg = $_.Exception.Message
+    }
+    if ($errorMsg) {
+        Write-Warning "Sync failed."
+        Write-Warning $errorMsg
+        exit
+    }
+    else {
+        Write-Host "Sync completed."
+        return $true
+        exit 0
+    }    
+    }
+#endregion
+#region Main Process
+try {
+    #region Sharepoint Sync
+    #Modify the siteID, webID, and listID via a F12 Network Capture of the Sync button on Chrome or Edge
+    #Modify the webURL with a link to the root of the Sharepoint site
+    #Modify the Tenant Title to the title of the Office 365 tenant (can review synch path for top level Sharepoint sync folder name)
+    #Modify the webTitle for the title of the Sharepoint site and the list Title for the title of the folder set being synched (likely Documents)
+    [mailaddress]$userUpn = cmd /c "whoami/upn"
+    [string]$userName= cmd /c "whoami"
+    $params = @{
+        #replace with data captured from your sharepoint site.
+        siteId    = "{00000000-0000-0000-0000-000000000000}"
+        webId     = "{00000000-0000-0000-0000-000000000000}"
+        listId    = "{00000000-0000-0000-0000-000000000000}"
+        webUrl    = "https://example.sharepoint.com/sites/SiteNameHere"
+        tenantTitle = "Manually enter the Tenant Name Here"
+        webTitle  = "Site Name from F12 grab"        
+        listTitle = "Liste Title from F12 grab"
+        userEmail = $userUpn
+        UserName = $UserName.TrimStart("azuread\")
+    }
+    $params.syncPath  = "C:\Users\$($params.userName)\$($params.tenantTitle)\$($params.webTitle) - $($Params.listTitle)"
+    $params.regPath = "HKCU:\SOFTWARE\Microsoft\OneDrive\Accounts\Business1\Tenants\$($params.tenantTitle)"
+    Write-Host "SharePoint params:"
+    $params | Format-Table
+    if (!(Get-ItemProperty $($params.regPath) -Name $($params.syncPath))) {
+        Write-Host "Sharepoint folder not found locally, will now sync.." -ForegroundColor Yellow
+        $sp = Sync-SharepointLocation @params
+        if (!($sp)) {
+            Throw "Sharepoint sync failed."
+        }
+    }
+    else {
+        Write-Host "Location already syncronized: $($params.syncPath)" -ForegroundColor Yellow
+        exit 0
+    }
+    #endregion
+}
+catch {
+    $errorMsg = $_.Exception.Message
+    }
+finally {
+    if ($errorMsg) {
+        Write-Warning $errorMsg
+        Throw $errorMsg
+        exit
+    }
+    else {
+        Write-Host "Completed successfully.."
+        exit 0
+    }
+}
+#endregion
